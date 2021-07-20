ac6c677b5e3208d9decd2f10c31a34f2b8d2ae98
Sarthak-Dayal <63827830+Sarthak-Dayal@users.noreply.github.com>
2021-07-19 23:18:19 -0500

    Add Code to visualize data

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ num_classes = 10
 (X_train, y_train), (X_test, y_test) = tf.keras.datasets.mnist.load_data()
 print('Shape of training images array:', X_train.shape)
 print('Shape of training labels array:', y_train.shape)
+
+# Visualizing data
+for i in range(9):
+    plt.subplot(3,3,i+1)
+    plt.imshow(X_train[i], cmap="gray", interpolation="none")
+    plt.title("Class {}".format(y_train[i]))
+
+plt.tight_layout()
+plt.show()
